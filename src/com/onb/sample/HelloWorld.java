d23268575969df217bee3008487b1bfd7d75e9b6
Anna Tan <it@debian.manila.orangeandbronze.com>
2013-06-05 13:23:35 +0800

    Nigga

--- src/com/onb/sample/HelloWorld.java
+++ src/com/onb/sample/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.onb.sample;
 
 public class HelloWorld {
-	private String message = "Hello world";
+	private String message = "Hello nigga";
 	
 	public String saySomeHello() {
 		return message;
