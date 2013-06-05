7f38a81a62bb7034ace9197c9a8e0c259287ffe3
Cherr Madayag <training@bootcamp-wheezy>
2013-06-05 14:09:32 +0800

    commit to expo

--- src/com/onb/sample/HelloWorld.java
+++ src/com/onb/sample/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	private String message = "Hello nigga world";
 	private String nigga = "Yoh nigga homie!";
+	private String newWord = "exposition";
 	
 	public String saySomeHello() {
 		return message;
