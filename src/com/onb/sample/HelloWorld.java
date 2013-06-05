419dfab10115c551161d249bb81917f7272b1f5c
Anna Tan <it@debian.manila.orangeandbronze.com>
2013-06-05 13:28:40 +0800

    Homie

--- src/com/onb/sample/HelloWorld.java
+++ src/com/onb/sample/HelloWorld.java
@@ -1,11 +1,9 @@
 package com.onb.sample;
 
 public class HelloWorld {
-<<<<<<< HEAD
-	private String message = "Hello nigga";
-=======
+
 	private String message = "Hello freaky world";
->>>>>>> 9e40dfc273427cba9f2ed33e2a527a98e14d919d
+	private String nigga = "Yoh nigga homie!";
 	
 	public String saySomeHello() {
 		return message;
