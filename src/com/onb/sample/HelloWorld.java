4d11750cb10f0fd8864298cb93431206b984241a
Anna Tan <it@debian.manila.orangeandbronze.com>
2013-06-05 14:36:07 +0800

    bah

--- src/com/onb/sample/HelloWorld.java
+++ src/com/onb/sample/HelloWorld.java
@@ -5,9 +5,6 @@ public class HelloWorld {
 	private String message = "Hello nigga world";
 	private String nigga = "Yoh nigga homie!";
 	private String newWord = "exposition";
-	private String exp = "Experiment";
-	private String exp2 = "Experiment2";
-	private String exp3 = "Experiment3";
 	
 	public String saySomeHello() {
 		return message;
