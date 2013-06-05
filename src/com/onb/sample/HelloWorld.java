13e513588a7ad98c9fa3dfc6d6db01d1781beae4
Anna Tan <it@debian.manila.orangeandbronze.com>
2013-06-05 14:03:41 +0800

    exp

--- src/com/onb/sample/HelloWorld.java
+++ src/com/onb/sample/HelloWorld.java
@@ -1,11 +1,10 @@
 package com.onb.sample;
 
 public class HelloWorld {
-<<<<<<< HEAD
-	private String message = "Hello nigga";
-=======
-	private String message = "Hello freaky world";
->>>>>>> 9e40dfc273427cba9f2ed33e2a527a98e14d919d
+
+	private String message = "Hello nigga world";
+	private String nigga = "Yoh nigga homie!";
+	private String exp = "Experiment";
 	
 	public String saySomeHello() {
 		return message;
